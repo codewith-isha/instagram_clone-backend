3b77490104f0a34f708eebe56ff1e40a4bf7137f
codewith-isha <codeoncanvas20@gmail.com>
2025-08-20 12:31:28 +0530

    login page

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,11 +1,12 @@
 const express = require("express");
-// const app = express
 const router = express.Router();
 const User = require("../models/auth");
 const bcrypt = require("bcrypt");
 const jwt = require("jsonwebtoken");
+const SECRETKEY = process.env.SECRETKEY
+const requireLogin = require('../middleware/requireLogin')
 
-// signup page 
+// signup page
 
 router.post("/singup", async (req, res) => {
   const { name, email, password } = req.body;
@@ -15,58 +16,57 @@ router.post("/singup", async (req, res) => {
       message: `Please fill all the fields !!`,
     });
   }
- try{
-  const savedUser =User.findOne({email:email})
-   
+  try {
+    const savedUser = User.findOne({ email: email });
     if (savedUser) {
       res.status(404).json({
         message: `User is already exists !!`,
-      })}
-      const hashedpassword = await bcrypt.hash(password,12)
-        const user = new User({
-          name: name,
-          email: email,
-          password: hashedpassword,
-        });
-         await user.save();
-
-        res.status(201).json({
-          message :"User added successfully !!"
-        })
-      }catch(error){
-        res.status(500).json({
-          message:`internal server error : ${error}`
-        })
-      }
-})
- 
-
-//  login page 
-
-router.post("/login", async(req, res) => {
-  const { email, password } = req.body;
-  if (!email || !password) {
-    res.status(422).json({
-      message: `Please add all the fields !!`,
+      });
+    }
+    const hashedpassword = await bcrypt.hash(password, 12);
+    const user = new User({
+      name: name,
+      email: email,
+      password: hashedpassword,
+    });
+    await user.save();
+    res.status(201).json({
+      message: "User added successfully !!",
+    });
+  } catch (error) {
+    res.status(500).json({
+      message: `internal server error : ${error}`,
     });
   }
+});
 
-  const dbUser =  await User.findOne({email:email})
+//  login page
+ router.post('/login', (req, res)=>{
+  const {email ,password }= req.body;
+  if(!email || !password){
+    return res.status(422).json({message : `Please fill all the fields !!`})
+  }
+  User.findOne({email :email}).then((dbuser)=>{
+    if(!dbuser){
+      return res.status(404).json({error:`User is not found with this email !!`})
+    }else{
+      bcrypt.compare(password , dbuser.password).then((doMatch)=>{
+        if(doMatch){
+          const token = jwt.sign ({_id: dbuser._id}, SECRETKEY)
+          res.status(200).json({message : `Congratulations you have successfully login your account`, token})
+        }
+        else{
+          return res.status(422).json({error:"Invalid credentials !!"})
+        }
+      })
+    }
+  })
+ })
 
-      if (!dbUser) {
-        res.status(404).json({
-          message: `User is not found with this id !!`,
-        });
-      } else {
-         jwt.sign(
-          {email },
-          process.env.SECRETKEY,
-          function (err, token) {
-            console.log(token);
-          }
-        );
-      }
-    })
-  
+ router.get('/protected', requireLogin, (req,res)=>{
+  return res.status(200).json({
+    message : `access granted`
+  })
+ })
 
 module.exports = router;
---
+++ middleware/requireLogin.js
@@ -0,0 +1,38 @@
+const jwt = require('jsonwebtoken')
+const SECRETKEY = process.env.SECRETKEY
+const User = require('../models/auth')
+
+// Middleware to check if the user is logged in 
+const requireLogin = (req,res,next)=>{
+  const bearerHeader = req.headers['authorization']
+  if(!bearerHeader){
+    res.status(403).json({
+      msg :`Please login first one !!`
+    })
+  }
+  const bearerToken = bearerHeader.split(" ");
+  const token = bearerToken[1];
+  jwt.verify(token, SECRETKEY , (err,payload)=>{
+    if(err){
+      res.status(403).json({
+        msg :`Please login first two`
+      })
+    }
+      const {_id} = payload;
+     User.findById(_id)
+     .then((dbuser)=>{
+      if(!dbuser){
+        return res.status(404).json({error : `User not found`})
+    }
+      req.user  = dbuser;
+      next()
+     })
+     .catch((err)=>{
+      res.status(500).json({error : `Internal Error !!`})
+     })
+  }
+
+
+)
+}
+module.exports = requireLogin;
